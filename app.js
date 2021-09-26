1ccde653b90a456dbb0fc616a8fc92ebd9df4a73
KierraWhit <89669379+KierraWhit@users.noreply.github.com>
2021-09-26 17:36:50 -0400

    added more if statements
    Code did not run at all. Added if statement to see if ran.

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 var ele =document.body.querySelector(".looper");
-var user_damage=0;
-var dragon_damage=0;
-var winner
-var loser
+var user_damage;
+var dragon_damage;
+var conditions = true;
+var dragon = 10;
+var user = 5;
+var question_2;
 
-while (user_damage<5 && dragon_damage<10) {
+
+
+while (conditions) {
     var question = Number(prompt("Enter a number to hit the dragon! Limit is 5: "));
-    if (question > 5) {
-        dragon_damage += 1;
-    } else {
-        dragon_damage = (Math.floor(Math.Random) * question) + 1
-
+    if ( question > 5 ) {
+        user = question - dragon_damage;
+        conditions = false;
+    }
+    while (conditions) {
+        question_2 = Math.random() * 10;
+        dragon = question_2 - user_damage;
+        if (dragon_damage === dragon | dragon_damage > dragon | dragon_damage < dragon);
+            conditions = false;
 
     }
-    if (dragon_damage >= 10 && user_damage < 5) {
-        ele.innerHTML = "You win"
-        winner = "User"
-        loser = "Dragon"
-    } else if (dragon_damage < 10 && user_damage >= 5) {
+for (user_damage = user; user_damage>dragon_damage; user++)
         ele.innerHTML = "You lose"
-        winner = "Dragon"
-        loser = "User"
-    }
-
-
+for (user_damage>user; user_damage<dragon_damage; user++)
+        ele.innerHTML = "You win"
 }
-
 
 
 
